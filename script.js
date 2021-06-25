6a7f3b96ba91a5b79679be742c6114bf52970055
papa-dilton <76129078+papa-dilton@users.noreply.github.com>
2021-06-25 00:33:27 -0500

    Version 0.2.1
    started JS to operate drop down menu for mobile, fixed horizontal scroll issue, and made topbar links look better.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log('JS is working!');
+var menuAccountedFor = false;
 //console.log('why hello, there, developer. Welcome to the console. I swear to god if you\'re debugging on internet expolorer you need to not. have fun.');
 
 function ready(callbackFunction){
@@ -16,4 +17,28 @@ function ready(callbackFunction){
             console.log('Click event listener working!');
             alert("This page will open in your default email pre-addressed to Sahil Chatwal. Send your contact information and any inquiries you may have.")
         }*/
+        window.addEventListener("load", function() {
+          window.dispatchEvent(new Event('resize'));
+      });
+       
+      window.onresize = function() {
+        if (window.innerWidth <= 669 && menuAccountedFor == false) {
+          
+          const menuLinesEl = document.getElementById('menuLines');
+          //Detects if menu lines have been clicked
+          menuLinesEl.addEventListener("click", function() {
+            console.log('element clicked!');
+            //Creates contact me menu item
+            const contMeDropEl = document.createElement('div');
+            contMeDropEl.innerHTML = 'Contact Me'
+            contMeDropEl.className = 'dropDiv'
+            menuLinesEl.appendChild(contMeDropEl)
+            //Detects if user clicks contact me
+            contMeDropEl.addEventListener("click", function() {
+              window.location.href = 'contact-me.html';
+            })
+          })
+          menuAccountedFor = true;
+        }
+        }
     })
